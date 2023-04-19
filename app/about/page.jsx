d3dd9adb1d7c44e3a78057e27f91d3dbbb37ee46
farukwebcode21 <farukahmadmk@yahoo.com>
2023-04-19 11:57:04 +0600

    start next crash course learning

---
+++ app/about/page.jsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import React from "react";
+
+export const metadata = {
+  title: "About Next js Metadata",
+};
+const HomePage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse doloremque
+        sapiente ab est odit at totam voluptatum voluptates excepturi suscipit
+        officiis inventore minima quod harum fuga autem, consequatur quis
+        facere! Dolorem voluptas exercitationem accusantium commodi sapiente
+        rem? Nisi et vel eius ipsum, qui, dolores, consequatur consectetur
+        pariatur possimus ex in quam voluptatem sunt reprehenderit reiciendis.
+        Doloribus voluptatem perferendis doloremque laborum. Necessitatibus,
+        voluptatem architecto ducimus quidem tempora laudantium adipisci.
+        Praesentium facilis laudantium eveniet saepe, beatae ipsum quae quo
+        repellat cum tempore voluptatem autem obcaecati exercitationem!
+        Distinctio optio incidunt quod illo voluptatem, tempore vero asperiores
+        quaerat quidem, eius facilis porro ducimus, aliquam numquam dicta dolor
+        assumenda et voluptatibus. Dolorem animi dignissimos vel, non doloremque
+        nam vero quidem eligendi dolores cupiditate laboriosam laborum incidunt
+        voluptatem id quo, ipsam voluptatibus? Iusto quae eius enim tempora, hic
+        dolorem nobis molestias dolore minima asperiores debitis explicabo harum
+        quisquam corporis, quam vel nisi assumenda odit laudantium eum deserunt?
+        Aperiam illo optio quibusdam eos sit alias soluta, quis maiores dolorum,
+        eius maxime, enim esse animi fugiat voluptatem? Doloremque repellat
+        omnis cumque corporis. Accusamus, quibusdam rem eligendi illo
+        cupiditate, aut eum ducimus blanditiis, porro iste non nisi! Magnam
+        obcaecati qui, amet voluptas delectus nostrum rem nisi eius omnis
+        temporibus soluta similique ipsa est voluptate rerum sint nulla officia
+        illum? Facilis velit aut, distinctio corrupti vel neque, quod
+        necessitatibus cum delectus odit perspiciatis quos illo molestias odio
+        id magnam sapiente non ex aliquam inventore praesentium quo placeat et!
+        At maiores repellendus ipsum aliquam eum dolores dolorem provident rerum
+        cumque doloremque.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>This is Team Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni eaque
+        delectus mollitia, aperiam nihil recusandae nemo eum eligendi dolor
+        dolore neque id nisi explicabo ad illo cupiditate at autem perferendis.
+        Natus, laborum necessitatibus, commodi id at non nostrum quod eaque
+        praesentium aperiam molestiae, doloribus expedita repellendus. Eveniet
+        similique magni, praesentium beatae, accusamus iste hic porro quibusdam
+        labore asperiores exercitationem, excepturi animi officia impedit odit.
+        Rem debitis assumenda sit libero eum minima laborum in vero odit
+        asperiores quam nesciunt inventore ratione deleniti, commodi ducimus
+        nisi ab hic delectus eligendi, nihil provident quasi odio veniam! Est
+        fugit, architecto odio dolor molestias quam!
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
