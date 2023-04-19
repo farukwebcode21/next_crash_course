5647d5c751099299272551a8cdce8a310b39c03f
farukwebcode21 <farukahmadmk@yahoo.com>
2023-04-19 12:21:45 +0600

    welcome for shall

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,10 +1,14 @@
+import Repo from "@/app/components/Repo";
+import Link from "next/link";
 import React from "react";
 
 const RepoPage = ({ params: { name } }) => {
   return (
     <div className="card">
-      <h2>{name}</h2>
-      <p>Repo Details</p>
+      <Link href="/code/repos" className="btn btn-back">
+        Back To Repositories
+      </Link>
+      <Repo name={name} />
     </div>
   );
 };
