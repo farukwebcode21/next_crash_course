4f800faddc9ae7f9f23f198db29ef90bee1b7dee
farukwebcode21 <farukahmadmk@yahoo.com>
2023-04-22 15:30:22 +0600

    add json data

--- app/api/courses/search/route.js
+++ app/api/courses/search/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import courses from "../data.json";
 export async function GET(request) {
-  const { searchParams } = new URL(request.url);
+  const { searchParams } = new URL(request.url);  
   const query = searchParams.get("query");
   const filterCourses = courses.filter((course) => {
     return course.title.toLowerCase().includes(query.toLowerCase());
